482af824b63c6644d448b878335ae48c6f8f9eee
s3xmaxman <badside3919@gmail.com>
2025-02-09 22:19:33 +0900

    Update splash screen configuration and remove old image

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,10 @@
 import { Stack } from "expo-router";
 import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { useCallback } from "react";
-import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const onLayoutRootView = useCallback(async () => {
-    await SplashScreen.hideAsync();
-  }, []);
-
   return (
-    <View
-      style={{ flex: 1, backgroundColor: "#000" }}
-      onLayout={onLayoutRootView}
-    >
+    <View style={{ flex: 1, backgroundColor: "#000" }}>
       <StatusBar style="light" />
       <Stack
         screenOptions={{
