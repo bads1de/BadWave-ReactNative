6bc1f1624a70acbcf1e6bb2cfc2c12cb1caec970
s3xmaxman <badside3919@gmail.com>
2026-06-03 13:53:09 +0900

    ⚙️ chore: react-native-reanimated/plugin を Babel 設定に追加

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,9 @@ module.exports = function (api) {
 
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["inline-import", { extensions: [".sql"] }]],
+    plugins: [
+      ["inline-import", { extensions: [".sql"] }],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
